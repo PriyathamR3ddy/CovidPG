a5bb2fa7efd1f547a045b53ab803a6331a94b066
Jaswanth Bommu <jaswanth.bommu@realpage.com>
2021-04-30 22:30:12 +0530

    test

---
+++ PGReservation/PGReservation/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace PGReservation.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PGReservation.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "PGReservation.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(PGReservation.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
