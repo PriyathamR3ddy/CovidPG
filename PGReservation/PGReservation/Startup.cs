db7f93a94373ae1c2373e6da71b1695bae561ef5
Jaswanth Bommu <jaswanth.bommu@realpage.com>
2021-04-30 21:58:35 +0530

    initial template

---
+++ PGReservation/PGReservation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PGReservation.Startup))]
+namespace PGReservation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
