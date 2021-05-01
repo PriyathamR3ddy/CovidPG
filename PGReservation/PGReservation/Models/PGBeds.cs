731f8bc0afce9a0bab13db5ae4d5acb22a16bd7e
priyatham.ponnolu@gmail.com <priyatham.ponnolu@gmail.com>
2021-05-01 21:33:29 +0530

    modified files

--- PGReservation/PGReservation/Models/PGBeds.cs
+++ PGReservation/PGReservation/Models/PGBeds.cs
@@ -9,8 +9,11 @@ namespace PGReservation.Models
         // public int PGId { set; get; }
         [Key]
         public int BedID { set; get; }
+        [Display(Name = "Bed No.")]
         public string BedNo { set; get; }
+        [Display(Name = "Bed Status")]
         public string BedStatus { get; set; }
+        [Display(Name = "Pg Registration")]
         public virtual PGRegistration PgRegistration { get; set; }
     }
 }
--- PGReservation/PGReservation/Models/PGRegistration.cs
+++ PGReservation/PGReservation/Models/PGRegistration.cs
@@ -8,7 +8,9 @@ namespace PGReservation.Models
     {
         [Key]
         public int PGID { set; get; }
+        [Display(Name = "PG Name")]
         public string PGName { set; get; }
+        [Display(Name = "Contact Person")]
         public string ContactPerson { set; get; }
         public string Phone { set; get; }
         public string Address { set; get; }
@@ -16,7 +18,9 @@ namespace PGReservation.Models
         public  string District { set; get; }
         public string  City { set; get; }
         public string PinCode { set; get; }
+        [Display(Name = "GMap Location")]
         public string   GmapLocation { set; get; }
+        [Display(Name = "No.of Beds")]
         public string NoOfBeds { set; get; }
 
         public string UserId { get; set; }
